1dde4a03a2a083d6d152dc960db92f8f62ca51ae
Sabri ÇAKIR <computerman353@gmail.com>
2022-05-22 22:57:58 +0300

    add controller and views

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,49 @@
+class MessagesController < ApplicationController
+    before_action :find_message, only: [:show, :edit, :update, :destroy]
+    def index
+        @messages = Message.all.order("created_at DESC")
+    end
+    
+    def new
+        @message = Message.new
+    end
+    
+    def show
+        
+    end
+    
+    def create
+        @message = Message.new(message_params)
+        if @message.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if @message.update(message_params)
+            redirect_to root_path
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @message.destroy
+            redirect_to root_path
+    end
+    
+    private
+
+    def message_params
+        params.require(:message).permit(:title, :description)
+    end
+
+    def find_message
+        @message = Message.find(params[:id])
+    end
+end
